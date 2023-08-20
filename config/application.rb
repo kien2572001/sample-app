c07bc667ec3939e8e2f4ae430b02553323d76a6a
kien2572001 <kien2572001.ntk@gmail.com>
2023-08-20 20:57:19 +0700

    feat: chapter 3 4 5

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module RailsTutorial
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :en # Ngôn ngữ mặc định
+    config.i18n.available_locales = [:en, :vi] # Danh sách các ngôn ngữ hỗ trợ
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
