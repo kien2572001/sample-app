a17ac23d25e0171495b1b5ac54ae8934031023de
kien2572001 <kien2572001.ntk@gmail.com>
2023-08-22 10:29:31 +0700

    fix: change '' to

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   constraints(locale: /en|vi/) do
     # Define your routes here
     resources :products
-    get 'demo_partials/new'
-    get 'demo_partials/edit'
-    get 'static_pages/home'
-    get 'static_pages/help'
+    get "demo_partials/new"
+    get "demo_partials/edit"
+    get "static_pages/home"
+    get "static_pages/help"
     # ...
   end
 end
